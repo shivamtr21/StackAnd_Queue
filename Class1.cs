ea6a358bc56d64eabc4983cc96ea9cae93e83012
shivamtr21 <shivamtripathi26297@gmail.com>
2023-06-22 23:58:32 +0530

    [Shivam Tripathi] : Stack and Queue Problem

---
+++ Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class Class1
+    {
+         
+        
+            public Node top;
+            public void StackWithLinkedList()
+            {
+                this.top = null;
+            }
+
+            public void push(int value)
+            {
+                Node node = new Node(value);
+                if (this.top == null)
+                {
+                    node.next = null;
+                }
+                else
+                {
+                    node.next = this.top;
+                }
+                this.top = node;
+                Console.WriteLine("{0} pushed to stack", value);
+            }
+
+
+            public void Display()
+            {
+                Node temp = this.top;
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using static StackAndQueue.Class1;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Class1 cls = new Class1();
+
+
+            
+            cls.push(70);
+            cls.push(30);
+
+            cls.push(50);
+
+            cls.Display();
+
+
+
+
+        }
+    }
+}
